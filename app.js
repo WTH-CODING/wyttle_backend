de10fb6f4a5b2288c78787f0663683c96fbe4058
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-07-04 12:36:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,4 +7,7 @@ dotenv.config({ path: "server/config/config.env" });
 const auth = require("./routes/auth");
 
 app.use("/api/v1/", auth);
+app.get("/", (req, res) => {
+    res.send("baba boi");
+  });
 module.exports = app;
