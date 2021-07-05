00b46b1bc16c1bb9dcf8753f70c7480fc26b8285
jason7531 <54910136+jason7531@users.noreply.github.com>
2021-07-05 20:39:07 +0530

    order and product routes modified

--- routes/order.js
+++ routes/order.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  newOrder,
-  getSingleOrder,
-  myOrders,
-} = require("../controllers/orderController");
+const controller = require("../controllers/order");
 
 const { verify } = require("../middlewares/verifyToken");
 
-router.post("/order/new", verify, newOrder);
-router.get("/order/:id", verify, getSingleOrder);
-router.get("/orders/me", verify, myOrders);
+router.post("/order/new", verify, controller.newOrder);
+router.get("/order/:id", verify, controller.getSingleOrder);
+router.get("/orders/me", verify, controller.myOrders);
 
 module.exports = router;
--- routes/product.js
+++ routes/product.js
@@ -1,20 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  getProducts,
-  newProduct,
-  createProductReview,
-  getProductReviews,
-  deleteReview,
-} = require("../controllers/product");
+const controller = require("../controllers/product");
 
 const { verify } = require("../middlewares/verifyToken");
 
-router.get("/products", getProducts);
+router.get("/products", controller.getProducts);
 
-router.put("/review", verify, createProductReview);
-router.get("/reviews", getProductReviews);
-router.delete("/reviews", verify, deleteReview);
+router.put("/review", verify, controller.createProductReview);
+router.get("/reviews", controller.getProductReviews);
+router.delete("/reviews", verify, controller.deleteReview);
 
 module.exports = router;
