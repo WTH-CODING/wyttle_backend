9f4d100f8877ba09b86a9fae14a2c07c51147097
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-07-06 10:09:08 +0530

    Update verifyToken.js

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
 //Middleware function for private routes
 exports.verify = function (req, res, next) {
-  // const token = req.header("auth-token");
-  const token = req.cookies.token;
+  const token = req.header("auth-token");
+  // const token = req.cookies.token;
   if (!token) return res.status(401).send("Access Denied");
 
   try {
