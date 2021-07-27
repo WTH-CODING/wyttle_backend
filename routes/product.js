45d13b3c877b930d139c2b5cf5df17e9cfdec9f2
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-07-27 10:47:54 +0530

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -8,13 +8,21 @@ const { verify } = require("../middlewares/verifyToken");
 
 router.get("/", product.getProducts);
 router.get("/category/:id", product.getProductsByCategory);
-router.get("/count", verify ,product.productCount);
+// router.get("/count", verify ,product.productCount);
+// router.get("/home", product.getHomeProducts);
+// router.get("/:id", product.getSingleProduct);
+// router.post("/", verify ,product.createProduct);
+// router.post("/" ,product.createProduct);
+// router.put("/", verify ,product.updateProduct);
+// router.delete("/:id", verify ,product.deleteProduct);
+
+router.get("/count", product.productCount);
 router.get("/home", product.getHomeProducts);
 router.get("/:id", product.getSingleProduct);
-// router.post("/", verify ,product.createProduct);
+router.post("/", product.createProduct);
 router.post("/" ,product.createProduct);
-router.put("/", verify ,product.updateProduct);
-router.delete("/:id", verify ,product.deleteProduct);
+router.put("/", product.updateProduct);
+router.delete("/:id", product.deleteProduct);
 
 
 module.exports = router;
